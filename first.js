03824e9a0821e7f9ce86a956639da77af10edac3
Rajdwip-69 <143189655+Rajdwip-69@users.noreply.github.com>
2023-11-18 11:42:39 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,40 @@
+// console.log("Hello World!");
+
+// let a = 10;
+// console.log(a);
+
+// const b = 10;
+// console.log(b);
+
+// var fullName = "Rajdwip";
+// console.log(fullName);
+
+
+// let v = 10;
+// let n = 20;
+// let c = v+n;
+// console.log(c);
+
+
+// let age = 20;
+// if(age>18){
+//     console.log("Pass!");
+// }
+
+// else {
+//     console.log("Fail");
+// }
+
+
+// Creating Object in JavaScript
+
+
+let student = {
+        fNmae:"Rajdwip Ghosh", 
+        age : 22,
+        cgpa : 9.2,
+        isPass : true,
+
+};
+
+console.log(student);
